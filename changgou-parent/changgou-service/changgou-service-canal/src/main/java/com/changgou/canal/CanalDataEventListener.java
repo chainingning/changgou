b101ebe0ae5e677be91c78bc4b0ae1d657cc3cff
chaining <ning.chai@foxmail.com>
2020-08-20 14:42:04 +0800

    同步实现

--- changgou-parent/changgou-service/changgou-service-canal/src/main/java/com/changgou/canal/CanalDataEventListener.java
+++ changgou-parent/changgou-service/changgou-service-canal/src/main/java/com/changgou/canal/CanalDataEventListener.java
@@ -1,11 +1,19 @@
 package com.changgou.canal;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.otter.canal.protocol.CanalEntry;
+import com.changgou.content.feign.ContentFeign;
+import com.changgou.content.pojo.Content;
+import com.changgou.entity.Result;
 import com.xpand.starter.canal.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.List;
 
 /**
  * @ClassName CanalDataEventListener
- * @Description:
+ * @Description: 监听类 监听数据的变化做处理
  * @Author ning.chai@foxmail.com
  * @Date 2020/8/19 0019
  * @Version V1.0
@@ -13,53 +21,101 @@ import com.xpand.starter.canal.annotation.*;
 @CanalEventListener
 public class CanalDataEventListener {
 
+    @Autowired
+    private ContentFeign contentFeign;
 
-    /**
-     * 监听新增操作
-     * @param eventType 当前操作类型
-     * @param rowData 发生变更的一行数据
-     */
-    @InsertListenPoint
-    public void  onEventInsert(CanalEntry.EventType eventType, CanalEntry.RowData rowData){
-        rowData.getAfterColumnsList().forEach(column -> {
-            System.out.println("onEventInsert--"+column.getName()+"::"+column.getValue());
-        });
-    }
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    //当数据被添加的时候触发
+    // CanalEntry.EventType eventType  监听到的操作的类型  INSERT  UPDATE ,DELETE ,CREATE INDEX ,GRAND
+    // CanalEntry.RowData rowData 被修改的数据()
+ /*   @InsertListenPoint
+    public void onEvent(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
+        //do something...
 
-    /***
-     * 修改数据监听
-     * @param rowData
-     */
-    @UpdateListenPoint
-    public void onEventUpdate(CanalEntry.RowData rowData) {
-        System.out.println("UpdateListenPoint");
-        rowData.getAfterColumnsList().forEach((c) -> System.out.println("By--Annotation: " + c.getName() + " ::   " + c.getValue()));
-    }
+        List<CanalEntry.Column> afterColumnsList = rowData.getAfterColumnsList();
+        for (CanalEntry.Column column : afterColumnsList) {
+            System.out.println(column.getName()+":"+column.getValue());
+        }
+    }*/
+    //当数据被更新的时候触发
+    /*@UpdateListenPoint
+    public void onEvent1(CanalEntry.RowData rowData) {
+        //do something...
+        List<CanalEntry.Column> afterColumnsList = rowData.getAfterColumnsList();
+        for (CanalEntry.Column column : afterColumnsList) {
+            System.out.println(column.getName()+":"+column.getValue());
+        }
+    }*/
+    // 当数据被删除的时候触发
+   /* @DeleteListenPoint
+    public void onEvent3(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
+        //do something...
+        List<CanalEntry.Column> afterColumnsList = rowData.getAfterColumnsList();
+        for (CanalEntry.Column column : afterColumnsList) {
+            System.out.println(column.getName()+":"+column.getValue());
+        }
+    }*/
 
-    /***
-     * 删除数据监听
-     * @param eventType
-     */
-    @DeleteListenPoint
-    public void onEventDelete(CanalEntry.EventType eventType) {
-        System.out.println("DeleteListenPoint");
-    }
+    //自定义事件的触发
+    // destination = "example" 指定某一个目的地 一定要和配置文件中的目录保持一致
+    //schema = "canal-test" 要监听的数据库实例
+    //table = {"t_user", "test_table"},   要监听的表
+    // eventType = CanalEntry.EventType.UPDATE  要监听的类型
+    /*@ListenPoint(destination = "example", schema = "changgou_content", table = {"tb_content"}, eventType = {CanalEntry.EventType.UPDATE,CanalEntry.EventType.INSERT,CanalEntry.EventType.DELETE})
+    public void onEvent4(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
+        //do something...
+
+        List<CanalEntry.Column> afterColumnsList = rowData.getAfterColumnsList();
+        for (CanalEntry.Column column : afterColumnsList) {
+            System.out.println(column.getName()+":"+column.getValue());
+        }
+    }*/
 
     /**
      * 自定义数据修改监听
+     *
      * @param eventType
      * @param rowData
      */
-    @ListenPoint(
-            destination = "example",
-            schema = "changgou_content",
-            table = {"tb_content_category"},
-            eventType = CanalEntry.EventType.UPDATE
+    @ListenPoint(destination = "example", schema = "changgou_content", table = {"tb_content"}, eventType = {CanalEntry.EventType.UPDATE, CanalEntry.EventType.INSERT, CanalEntry.EventType.DELETE})
+    public void onEventCustomUpdate(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
+        //1.获取到被修改的category_id
+        String categoryId = getColumnValue(eventType, rowData);
 
-    )
-    public void onEventCustomUpdate(CanalEntry.EventType eventType,CanalEntry.RowData rowData){
-        System.err.println("DeleteListenPoint");
-        rowData.getAfterColumnsList().forEach((c) -> System.out.println("By--Annotation: " + c.getName() + " ::   " + c.getValue()));
+        //2.调用feign 获取数据
+        Result<List<Content>> byCategory = contentFeign.findByCategory(Long.valueOf(categoryId));
+        //3.存储到redis中
+        List<Content> data = byCategory.getData();//List
+        //4.
+        stringRedisTemplate.boundValueOps("content_" + categoryId).set(JSON.toJSONString(data));
+    }
+
+    private String getColumnValue(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
+        //1.判断更改类型 如果是删除 则需要获取到before的数据
+        String categoryId = "";
+        if (CanalEntry.EventType.DELETE == eventType) {
+            List<CanalEntry.Column> beforeColumnsList = rowData.getBeforeColumnsList();
+            for (CanalEntry.Column column : beforeColumnsList) {
+                //column.getName(列的名称   column.getValue() 列对应的值
+                if (column.getName().equals("category_id")) {
+                    categoryId = column.getValue();
+                    return categoryId;
+                }
+            }
+        } else {
+            //2判断是 更新 新增 获取after的数据
+            List<CanalEntry.Column> beforeColumnsList = rowData.getAfterColumnsList();
+            for (CanalEntry.Column column : beforeColumnsList) {
+                //column.getName(列的名称   column.getValue() 列对应的值
+                if (column.getName().equals("category_id")) {
+                    categoryId = column.getValue();
+                    return categoryId;
+                }
+            }
+        }
+        //3.返回
+        return categoryId;
     }
 
 }
