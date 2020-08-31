0fb05f2be37639207cd73c6cca310e5872d0f2c9
chaining <ning.chai@foxmail.com>
2020-08-31 16:57:49 +0800

    关键词搜索测试实现

--- changgou-parent/changgou-service/changgou-service-search/src/main/java/com/changgou/search/controller/SkuController.java
+++ changgou-parent/changgou-service/changgou-service-search/src/main/java/com/changgou/search/controller/SkuController.java
@@ -4,10 +4,9 @@ import com.changgou.entity.Result;
 import com.changgou.entity.StatusCode;
 import com.changgou.search.service.SkuService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 /**
  * @ClassName SkuController
@@ -23,6 +22,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class SkuController {
     @Autowired
     private SkuService skuService;
+
+
+    /**
+     * 搜索
+     * @param searchMap
+     * @return
+     */
+    @PostMapping
+    public Map search(@RequestBody(required = false) Map searchMap){
+        return  skuService.search(searchMap);
+    }
 
     /**
      * 导入数据
