21fb180cffbdd6bc38a3d0a773c4bb8d5da21967
chaining <ning.chai@foxmail.com>
2020-08-31 14:59:00 +0800

    生成规格动态域

--- changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/GoodsApplication.java
+++ changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/GoodsApplication.java
@@ -16,7 +16,7 @@ import tk.mybatis.spring.annotation.MapperScan;
  **/
 @SpringBootApplication
 @EnableEurekaClient
-@MapperScan(basePackages = {"com.changgou.dao"})
+@MapperScan(basePackages = {"com.changgou.goods.dao"})
 public class GoodsApplication {
     public static void main(String[] args) {
         SpringApplication.run(GoodsApplication.class);
--- changgou-parent/changgou-service/changgou-service-search/src/main/java/com/changgou/search/service/impl/SkuServiceImpl.java
+++ changgou-parent/changgou-service/changgou-service-search/src/main/java/com/changgou/search/service/impl/SkuServiceImpl.java
@@ -8,8 +8,10 @@ import com.changgou.search.dao.SkuEsMapper;
 import com.changgou.search.pojo.SkuInfo;
 import com.changgou.search.service.SkuService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @ClassName SkuServiceImpl
@@ -18,6 +20,7 @@ import java.util.List;
  * @Date 2020/8/27 0027
  * @Version V1.0
  **/
+@Service
 public class SkuServiceImpl implements SkuService {
 
     @Autowired
@@ -34,6 +37,15 @@ public class SkuServiceImpl implements SkuService {
 
         //将List<Sku>转成List<SkuInfo>
         List<SkuInfo> skuInfoList = JSON.parseArray(JSON.toJSONString(skuResult.getData()),SkuInfo.class);
+
+        for (SkuInfo skuInfo : skuInfoList) {
+            //获取spec->Map(String)->Map类型({"电视音响效果":"立体声","电视屏幕尺寸":"20英寸","尺码":"165"})
+            Map<String,Object> specMap = JSON.parseObject(skuInfo.getSpec(),Map.class);
+            //如果需要生成动态的域，只需要将该域存入到一个Map<String,Object>对象中即可，该Map<String,Object>的key会生成一个域，域的名字为该map的key
+            //当前Map<String,Object>后面Object的值会作为当前Ｓｋｕ对象该域（key)对应的值
+            skuInfo.setSpecMap(specMap);
+        }
+
 
         //调用Dao实现数据批量导入
         skuEsMapper.saveAll(skuInfoList);
