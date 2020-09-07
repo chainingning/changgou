1bdf4c9a3ef0b5d3c7458f61fc7c25cda1ce8bec
chaining <ning.chai@foxmail.com>
2020-09-07 15:53:08 +0800

    规格分组合并查询

--- changgou-parent/changgou-service/changgou-service-search/src/main/java/com/changgou/search/service/impl/SkuServiceImpl.java
+++ changgou-parent/changgou-service/changgou-service-search/src/main/java/com/changgou/search/service/impl/SkuServiceImpl.java
@@ -1,10 +1,12 @@
 package com.changgou.search.service.impl;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.changgou.entity.Result;
 import com.changgou.goods.feign.SkuFeign;
 import com.changgou.goods.pojo.Brand;
 import com.changgou.goods.pojo.Sku;
+import com.changgou.goods.pojo.Spec;
 import com.changgou.search.dao.SkuEsMapper;
 import com.changgou.search.pojo.SkuInfo;
 import com.changgou.search.service.SkuService;
@@ -22,10 +24,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @ClassName SkuServiceImpl
@@ -52,6 +52,7 @@ public class SkuServiceImpl implements SkuService {
 
     /**
      * 条件搜索
+     *
      * @param searchMap
      * @return Map
      */
@@ -90,7 +91,6 @@ public class SkuServiceImpl implements SkuService {
         AggregatedPage<SkuInfo> page = elasticsearchTemplate.queryForPage(query, SkuInfo.class);
 
 
-
         //分析数据
 
         //分页参数-总记录数
@@ -103,9 +103,9 @@ public class SkuServiceImpl implements SkuService {
 
         //封装一个Map存储所有数据，并返回
         HashMap<String, Object> resultMap = new HashMap<>();
-        resultMap.put("rows",contents);
-        resultMap.put("total",totalElements);
-        resultMap.put("totalPages",totalPages);
+        resultMap.put("rows", contents);
+        resultMap.put("total", totalElements);
+        resultMap.put("totalPages", totalPages);
 
         //分类分组查询实现
         List<String> categoryList = searchCategoryList(nativeSearchQueryBuilder);
@@ -115,11 +115,14 @@ public class SkuServiceImpl implements SkuService {
         List<String> brandList = searchBrandList(nativeSearchQueryBuilder);
         resultMap.put("brandList", brandList);
 
+        //规格查询
+
         return resultMap;
     }
 
     /**
      * 分类分组查询
+     *
      * @param nativeSearchQueryBuilder
      * @return
      */
@@ -132,7 +135,6 @@ public class SkuServiceImpl implements SkuService {
          */
         nativeSearchQueryBuilder.addAggregation(AggregationBuilders.terms("skuCategory").field("categoryName").size(50));
         AggregatedPage<SkuInfo> aggregatedPage = elasticsearchTemplate.queryForPage(nativeSearchQueryBuilder.build(), SkuInfo.class);
-
 
 
         /**
@@ -155,6 +157,7 @@ public class SkuServiceImpl implements SkuService {
 
     /**
      * 品牌分组查询
+     *
      * @param nativeSearchQueryBuilder
      * @return
      */
@@ -167,7 +170,6 @@ public class SkuServiceImpl implements SkuService {
          */
         nativeSearchQueryBuilder.addAggregation(AggregationBuilders.terms("skuBrand").field("brandName").size(50));
         AggregatedPage<Brand> aggregatedPage = elasticsearchTemplate.queryForPage(nativeSearchQueryBuilder.build(), Brand.class);
-
 
 
         /**
@@ -188,6 +190,75 @@ public class SkuServiceImpl implements SkuService {
     }
 
 
+    /**
+     * 规格分组查询
+     *
+     * @param nativeSearchQueryBuilder
+     * @return
+     */
+    private List<String> searchSpecList(NativeSearchQueryBuilder nativeSearchQueryBuilder) {
+        /**
+         * 分组查询规格集合
+         * 1.addAggregation():添加一个聚合操作
+         * terms:取别名
+         * field:根据哪个域进行分组
+         * size如果不加，默认就是10条
+         */
+        nativeSearchQueryBuilder.addAggregation(AggregationBuilders.terms("skuSpec").field("spec.keyword").size(10000));
+        AggregatedPage<Spec> aggregatedPage = elasticsearchTemplate.queryForPage(nativeSearchQueryBuilder.build(), Spec.class);
+
+
+        /**
+         * 获取分组结果
+         * aggregatePage.getAggregation():获取的是集合，可以根据多个域进行分组
+         * .get("skuSpec")：获取指定域的集合数据 [{"电视音响效果":"小影院","电视屏幕尺寸":"20英寸","尺码":"165"}
+         * {"电视音响效果":"立体声","电视屏幕尺寸":"20英寸","尺码":"170"}
+         * ]
+         */
+        StringTerms stringTerms = aggregatedPage.getAggregations().get("skuSpec");
+        List<String> specList = new ArrayList<>();
+
+        if (stringTerms != null) {
+            for (StringTerms.Bucket bucket : stringTerms.getBuckets()) {
+                String keyAsString = bucket.getKeyAsString();//其中的一个分类名字
+                specList.add(keyAsString);
+            }
+        }
+
+        //合并后的Map对象
+        HashMap<String, Set<String>> allSpec = new HashMap<>();
+
+
+        //1.循环specList  spec={"电视音响效果":"立体声","电视屏幕尺寸":"20英寸","尺码":"165"}
+        for (String spec : specList) {
+
+            // 2.将每个json字符串转成Map<String,String>
+            Map<String,String> specMap = JSON.parseObject(spec, Map.class);
+
+            //3.将每个Map对象合并成一个Map<String,Set<String>>
+            /**
+             * 4.合并流程
+             */
+            for (Map.Entry<String, String> entry : specMap.entrySet()) {
+                //4.1取出当前Map,并且获取对应的key，以及对应的value
+                String key = entry.getKey();//规格名字
+                String spceValue = entry.getValue();//规格值
+
+                //4.2将当前循环的数据合并到一个Map<String,Set<String>>中
+                //从allSpec中获取当前规格对应的Set集合数据
+                Set<String> specSet = allSpec.get(key);
+                if (CollectionUtils.isEmpty(specSet)) {
+                    //之前allSpec中没有数据
+                    specSet = new HashSet<>();
+                }
+                specSet.add(spceValue);
+                allSpec.put(key,specSet);
+            }
+
+        }
+        return specList;
+    }
+
 
     @Override
     public void importData() {
@@ -196,11 +267,11 @@ public class SkuServiceImpl implements SkuService {
 
 
         //将List<Sku>转成List<SkuInfo>
-        List<SkuInfo> skuInfoList = JSON.parseArray(JSON.toJSONString(skuResult.getData()),SkuInfo.class);
+        List<SkuInfo> skuInfoList = JSON.parseArray(JSON.toJSONString(skuResult.getData()), SkuInfo.class);
 
         for (SkuInfo skuInfo : skuInfoList) {
             //获取spec->Map(String)->Map类型({"电视音响效果":"立体声","电视屏幕尺寸":"20英寸","尺码":"165"})
-            Map<String,Object> specMap = JSON.parseObject(skuInfo.getSpec(),Map.class);
+            Map<String, Object> specMap = JSON.parseObject(skuInfo.getSpec(), Map.class);
             //如果需要生成动态的域，只需要将该域存入到一个Map<String,Object>对象中即可，该Map<String,Object>的key会生成一个域，域的名字为该map的key
             //当前Map<String,Object>后面Object的值会作为当前Ｓｋｕ对象该域（key)对应的值
             skuInfo.setSpecMap(specMap);
