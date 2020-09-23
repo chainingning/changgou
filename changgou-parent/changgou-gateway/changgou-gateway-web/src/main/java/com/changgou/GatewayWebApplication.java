22bc6b808ef98a5b59f6982afa81c762d65a7b7d
chaining <ning.chai@foxmail.com>
2020-09-23 15:07:51 +0800

    微服务网关限流

--- changgou-parent/changgou-gateway/changgou-gateway-web/src/main/java/com/changgou/GatewayWebApplication.java
+++ changgou-parent/changgou-gateway/changgou-gateway-web/src/main/java/com/changgou/GatewayWebApplication.java
@@ -2,7 +2,11 @@ package com.changgou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 /**
  * @ClassName GatewayWebApplication
@@ -16,5 +20,19 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class GatewayWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayWebApplication.class,args);
+    }
+
+    /**
+     * 创建用户唯一标识，使用IP作为用户唯一标识，根据IP进行限流操作
+     */
+    @Bean(name = "ipKeyResolver")
+    public KeyResolver userKeyResolver(){
+        return new KeyResolver() {
+            @Override
+            public Mono<String> resolve(ServerWebExchange exchange) {
+                String hostString = exchange.getRequest().getRemoteAddress().getHostString();
+                return Mono.just(hostString);
+            }
+        };
     }
 }
